e7f1000a2d8ee304a9ac7627edc82a78ad3c7e52
aratasato <14242642+aratasato@users.noreply.github.com>
2019-10-29 08:10:01 +0900

    add Conn struct

--- db/db.go
+++ db/db.go
@@ -5,7 +5,6 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
 	"github.com/pkg/errors"
-	"log"
 )
 
 type Questions struct {
@@ -22,39 +21,38 @@ type QAndA struct {
 	CreatedAt string  `db:"created_at"`
 }
 
-var (
-	db *sqlx.DB
-)
+type Conn struct {
+	conn *sqlx.DB
+}
 
-func init() {
+func NewDB() (*Conn, error) {
 	var err error
-	db, err = sqlx.Open(
+	db, err := sqlx.Open(
 		"mysql",
 		"aratasato:hoge@tcp(127.0.0.1:3306)/geing",
 	)
-	if err != nil {
-		log.Fatalln(err)
-	}
+
+	return &Conn{db}, err
 }
 
 // 質問を追加
-func CreateQuestion(body string) error {
+func (db *Conn) CreateQuestion(body string) error {
 	fmt.Println(body)
-	tx := db.MustBegin()
+	tx := db.conn.MustBegin()
 	tx.MustExec("INSERT INTO qandas (question) VALUES (?)", body)
 	err := tx.Commit()
 	return errors.WithMessage(err, "failed: add question")
 }
 
 // 質問回答セットを1件取得
-func GetQA(id int) (QAndA, error) {
+func (db *Conn) GetQA(id int) (QAndA, error) {
 	qa := QAndA{}
-	err := db.Get(&qa, "SELECT * FROM qandas WHERE id = ?", id)
+	err := db.conn.Get(&qa, "SELECT * FROM qandas WHERE id = ?", id)
 	return qa, errors.WithMessage(err, "failed: get qa")
 }
 
-func GetQuestions(page int) ([]Questions, error) {
+func (db *Conn) GetQuestions(page int) ([]Questions, error) {
 	var questions []Questions
-	err := db.Select(&questions, "SELECT id, question, created_at FROM qandas WHERE id > ? * 10 LIMIT 20", page)
+	err := db.conn.Select(&questions, "SELECT id, question, created_at FROM qandas WHERE id > ? * 10 LIMIT 20", page)
 	return questions, errors.WithMessage(err, "failed: get question")
 }
