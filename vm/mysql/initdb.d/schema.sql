19d877e23c622d58a3e952ebaa56c1aac736d20a
aratasato <14242642+aratasato@users.noreply.github.com>
2019-11-14 23:27:36 +0900

    fix timezone

--- vm/mysql/initdb.d/schema.sql
+++ vm/mysql/initdb.d/schema.sql
@@ -3,5 +3,5 @@ CREATE TABLE qandas
     id         INT       NOT NULL AUTO_INCREMENT primary key,
     question   TEXT      NOT NULL,
     answer     text,
-    created_at TIMESTAMP NOT NULL
+    created_at DATETIME DEFAULT current_timestamp
 );
